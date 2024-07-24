c5edc1ee3f57fe6e12103c975edd76dda50c6a71
yinhaitao <yinht@jnsec.net>
2024-07-24 14:00:49 +0800

    LeetCode NO.11

---
+++ src/main/java/com/yin/algorithm/Lc11.java
@@ -0,0 +1,41 @@
+package com.yin.algorithm;
+
+/**
+ * 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+ *
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ *
+ * 返回容器可以储存的最大水量。
+ *
+ * 说明：你不能倾斜容器。
+ *
+ *
+ *
+ * 示例 1：
+ * 输入：[1,8,6,2,5,4,8,3,7]
+ * 输出：49
+ * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49
+ *
+ * 提示：
+ * n == height.length
+ * 2 <= n <= 10^5
+ * 0 <= height[i] <= 10^4
+ */
+public class Lc11 {
+}
+
+class Solution11 {
+    public int maxArea(int[] height) {
+        // 维护两个指针
+        int left = 0, right = height.length - 1, max = Math.min(height[left], height[right]) * (right - left);
+        while (left < right) {
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+            max = Math.max(Math.min(height[left], height[right]) * (right - left), max);
+        }
+        return max;
+    }
+}
